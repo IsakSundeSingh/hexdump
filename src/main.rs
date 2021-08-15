2243a8160f47345fcd628bb145db334223e47f1c
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2021-08-15 15:54:09 +0200

    Add working program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+
+const BYTES_PER_LINE: usize = 16;
+
+fn main() -> std::io::Result<()> {
+  let filename = std::env::args().nth(1).expect("expected filename");
+
+  let mut file = std::fs::File::open(filename)?;
+  let mut buffer = [0; BYTES_PER_LINE];
+
+  let mut pos = 0;
+  while file.read_exact(&mut buffer).is_ok() {
+    print!("[0x{:08x}] ", pos);
+    for byte in &buffer {
+      match *byte {
+        0x00 => print!(".  "),
+        0xff => print!("## "),
+        _ => print!("{:02x}", byte),
+      }
+    }
+    println!();
+    pos += BYTES_PER_LINE;
+  }
+
+  Ok(())
 }
